9abea84bd8620de19204f2a1126249cd3794c0b7
Devlin <devlin@devlinrocha.com>
2025-01-27 15:09:58 -0800

    add `--local` flag; rename `run` function to `run_module`; refactor file structure

--- src/create_lilypad_module/templates/scripts/run.py
+++ src/create_lilypad_module/templates/scripts/run.py
@@ -1,35 +1,61 @@
 import argparse
 import subprocess
 from ..config.constants import (
+    DOCKER_REPO,
     MODULE_REPO,
     TARGET_COMMIT,
     WEB3_DEVELOPMENT_KEY,
 )
 
-parser = argparse.ArgumentParser(
-    description="Run the Lilypad module with specified input."
-)
 
-parser.add_argument(
-    "input", type=str, help="The input to be processed by the Lilypad module."
-)
+def run_module():
+    parser = argparse.ArgumentParser(
+        description="Run the Lilypad module with specified input."
+    )
 
-args = parser.parse_args()
+    parser.add_argument(
+        "input", type=str, help="The input to be processed by the Lilypad module."
+    )
 
-input = args.input
+    parser.add_argument(
+        "--local",
+        help="Run the Lilypad module Docker image locally.",
+    )
 
-command = [
-    "lilypad",
-    "run",
-    f"{MODULE_REPO}:{TARGET_COMMIT}",
-    "--web3-private-key",
-    WEB3_DEVELOPMENT_KEY,
-    "-i",
-    f"input={input}",
-]
+    args = parser.parse_args()
 
-try:
-    result = subprocess.run(command, check=True, text=True)
-    print("Lilypad module executed successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"An error occurred: {e}")
+    input = args.input
+    local = args.local
+
+    command = (
+        [
+            "docker",
+            "run",
+            "-e",
+            f"INPUT_TEXT={input}",
+            "-v",
+            "$(pwd)/outputs:/outputs",
+            f"{DOCKER_REPO}:latest",
+        ]
+        if local
+        else [
+            "lilypad",
+            "run",
+            f"{MODULE_REPO}:{TARGET_COMMIT}",
+            "--web3-private-key",
+            WEB3_DEVELOPMENT_KEY,
+            "-i",
+            f"input={input}",
+        ]
+    )
+
+    try:
+        result = subprocess.run(command, check=True, text=True)
+        print("Lilypad module executed successfully.")
+        return result
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    run_module()
