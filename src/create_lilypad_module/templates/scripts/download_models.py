b09d0c2142f1be2d8d9478b5a43bf1711d8e08b7
Devlin <devlin@devlinrocha.com>
2025-01-28 19:38:24 -0800

    rename `download_model.py` to `download_models.py`

--- src/create_lilypad_module/templates/scripts/download_models.py
+++ src/create_lilypad_module/templates/scripts/download_models.py
@@ -1,13 +1,13 @@
 from config.constants import MODEL_URL
 
 
-def download_model():
+def download_models():
     # tokenizer = AutoTokenizer.from_pretrained(MODEL_URL)
     # model = AutoModelForSequenceClassification.from_pretrained(MODEL_URL)
 
-    tokenizer.save_pretrained("./model")
-    model.save_pretrained("./model")
+    tokenizer.save_pretrained("./models")
+    model.save_pretrained("./models")
 
 
 if __name__ == "__main__":
-    download_model()
+    download_models()
