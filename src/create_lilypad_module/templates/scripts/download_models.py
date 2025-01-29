662660f7f1cfdb6c8ff849167ff1a6e978e1622a
Devlin <devlin@devlinrocha.com>
2025-01-29 14:28:59 -0800

    refactor; provide hints to user

--- src/create_lilypad_module/templates/scripts/download_models.py
+++ src/create_lilypad_module/templates/scripts/download_models.py
@@ -1,12 +1,23 @@
-from config.constants import MODEL_URL
+# Download the model(s) and tokenizer(s) for your module from the Hugging Face model hub
+# https://huggingface.co/docs/hub/en/models-downloading
+
+
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
 def download_models():
-    # tokenizer = AutoTokenizer.from_pretrained(MODEL_URL)
-    # model = AutoModelForSequenceClassification.from_pretrained(MODEL_URL)
+    MODEL_IDENTIFIER = ""
 
-    tokenizer.save_pretrained("./models")
-    model.save_pretrained("./models")
+    try:
+        # tokenizer = AutoTokenizer.from_pretrained(MODEL_IDENTIFIER)
+        # model = AutoModelForSequenceClassification.from_pretrained(MODEL_IDENTIFIER)
+        tokenizer.save_pretrained("./models")
+        model.save_pretrained("./models")
+        print("Models downloaded successfully.")
+    except:
+        print("Error downloading models.")
+        print("Have you configured the download script?")
+        print("👉 /scripts/download_models.py")
 
 
 if __name__ == "__main__":
